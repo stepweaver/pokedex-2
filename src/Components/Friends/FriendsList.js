5895964214c2fc25117694b7ab65af084395d8f6
stepweaver <weaverst@mail.gvsu.edu>
2024-07-25 10:33:56 -0400

    We have a fancy FriendsList now!

--- src/Components/Friends/FriendsList.js
+++ src/Components/Friends/FriendsList.js
@@ -1,5 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { getFriends } from './friendsService';
+import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+
+const Demo = styled('div')(({ theme }) => ({
+  backgroundColor: 'transparent',
+  color: '#1f1f1f'
+}));
 
 const FriendsList = () => {
   const [friends, setFriends] = useState([]);
@@ -25,14 +39,34 @@ const FriendsList = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <div>
-      <h2>Friends List</h2>
-      <ul>
-        {friends.map((friend, index) => (
-          <li key={index}>{friend.username}</li>
-        ))}
-      </ul>
-    </div>
+    <Box sx={{ flexGrow: 1, maxWidth: 752 }}>
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+        Friends List
+      </Typography>
+      <Demo>
+        <List>
+          {friends.map((friend, index) => (
+            <ListItem key={index}>
+              <ListItemAvatar>
+                <Avatar>
+                  {friend.username.charAt(0).toUpperCase()}
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <Link 
+                    to={`/profile/${friend.id}`} 
+                    style={{ color: '#fff', textDecoration: 'none' }}
+                  >
+                    {friend.username}
+                  </Link>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Demo>
+    </Box>
   );
 };
 
